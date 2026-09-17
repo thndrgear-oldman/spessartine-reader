5316b97fa77db375cbb5c26232699d03bd4166c5
thndrgear-oldman <thndrgear@gmail.com>
2026-09-17 01:12:36 +0000

    Create mod.rs

---
+++ src/reader/mod.rs
@@ -0,0 +1,15 @@
+pub mod auto_detect;
+pub mod chapter_bundle;
+pub mod chapter_switch;
+pub mod input_handler;
+pub mod mode;
+pub mod mode_switch;
+pub mod preload;
+pub mod state;
+pub mod texture_handler;
+pub mod mode_actions;
+pub mod worker;
+
+pub use chapter_bundle::ChapterBundle;
+pub use state::ReaderState;
+
